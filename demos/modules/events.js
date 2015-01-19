a7ddabfefbea198f5c9893c6d9805ac11e7f14f9
shall <hall.steven.g@gmail.com>
2015-01-18 22:50:15 -0800

    update demos after events changes

--- demos/modules/events.js
+++ demos/modules/events.js
@@ -1,13 +1,13 @@
 var raycaster = new THREE.Raycaster();
 
-export function mouseDown(camera, selection, func) {
-  return function onMouseDown (event) {
+export function raycast(camera, selection, type) {
 
+  var listener = function(event) {
     var vector = new THREE.Vector3();
 
     var mouse = {
-      x: ((event.clientX - 1) / window.innerWidth) * 2 - 1,
-      y: -((event.clientY - 1) / window.innerHeight ) * 2 + 1
+      x:  ((event.clientX - 1) / window.innerWidth ) * 2 - 1,
+      y: -((event.clientY - 1) / window.innerHeight) * 2 + 1
     };
 
     vector.set(mouse.x, mouse.y , 0.5);
@@ -17,10 +17,12 @@ export function mouseDown(camera, selection, func) {
 
     var target = raycaster.intersectObjects(selection[0]);
 
-    if (target.length) { //NEED TO ABSTRACT THIS OUT
-      subunit.selectObject(target[0].object)
-        .attr("material", func);
+    if (target.length) {
+      target[0].type = type;
+      target[0].object.dispatchEvent(target[0]);
     }
-  };
-}
 
+  };
+
+  document.addEventListener(type, listener, false);
+}
