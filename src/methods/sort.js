0e23e9829b7222e205f7ff0d2d8af70ddd6def98
shall <hall.steven.g@gmail.com>
2015-01-23 09:36:41 -0800

    add sort method

---
+++ src/methods/sort.js
@@ -0,0 +1,21 @@
+
+export function sort(comparator) {
+  comparator = selection_sortComparator.apply(this, arguments);
+  for (var j = -1, m = this.length; ++j < m;) {
+    this[j].sort(comparator);
+  }
+  return this.order();
+}
+
+function selection_sortComparator(comparator) {
+  if (!arguments.length) {
+    comparator = ascending;
+  }
+  return function(a, b) {
+    return a && b ? comparator(a.__data__, b.__data__) : !a - !b;
+  };
+}
+
+function ascending(a, b) {
+  return a < b ? -1 : a > b ? 1 : a >= b ? 0 : NaN;
+}
