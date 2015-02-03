259d5090ed850deda05514732cfedc7fc024c78e
shall <hall.steven.g@gmail.com>
2015-02-02 19:19:01 -0800

    start text wrap

--- demos/modules/text.js
+++ demos/modules/text.js
@@ -33,3 +33,68 @@ export function makeSprite(text, color, points) {
     height: points + pad
   };
 }
+
+export function wrapText(text, color, points, maxWidth) {
+
+  var canvas = d3.select("body").append("canvas")
+    .style("display", "none");
+
+  var texture, context;
+  var testLine, testWidth;
+
+  var pad = points * 0.5;
+  var lineHeight = points;
+
+  var total = lineHeight;
+
+  canvas.attr({
+    width: maxWidth + (pad * 2),
+    height: 200
+  });
+
+  context = canvas.node().getContext("2d");
+  context.font = "normal " + points + "pt Arial";
+  context.fillStyle = color;
+
+  var words = text.split(" ")
+  var line  = "";
+
+  for(var n = 0; n < words.length; n++) {
+
+    testLine  = line + words[n] + " ";
+    testWidth = context.measureText(testLine).width;
+
+    if(testWidth > maxWidth) {
+      context.fillText(line, pad, total);
+      line = words[n] + " ";
+      total += lineHeight;
+    }
+    else {
+      line = testLine;
+    }
+  }
+  console.log(line, pad, total)
+  context.fillText(line, pad, total);
+
+  // var url = canvas.node().toDataURL();
+
+  texture = new THREE.Texture(canvas.node());
+  // texture = THREE.ImageUtils.loadTexture(url);
+
+  canvas.remove();
+
+  return { 
+    map: texture, 
+    width: maxWidth + (pad * 2), 
+    height: total + (pad * 2)
+  };
+}
+
+
+
+
+
+
+
+
+
