93e7f786a0a53896df05340de9e171f3206c87fc
shall <hall.steven.g@gmail.com>
2015-02-03 16:11:25 -0800

    re-factor text module, memoization

--- demos/modules/text.js
+++ demos/modules/text.js
@@ -1,6 +1,6 @@
 import { memoize } from 'modules/utils';
 
-export var labelCache = memoize(makeSprite);
+export var getLabel = memoize(makeSprite);
 
 export function makeSprite(text, color, points) {
   var canvas, texture, context, textWidth;
@@ -11,12 +11,12 @@ export function makeSprite(text, color, points) {
     .style("display", "none");
 
   context = canvas.node().getContext("2d");
-  context.font = "normal " + points + "pt Arial";
+  context.font = "normal " + points + "pt helvetica";
 
   textWidth = context.measureText(text).width + pad;
   canvas.attr({width: textWidth, height: points + pad});
 
-  context.font = "normal " + points + "pt Arial";
+  context.font = "normal " + points + "pt helvetica";
   context.textAlign    = "center";
   context.textBaseline = "middle";
   context.fillStyle    = color;
@@ -24,8 +24,6 @@ export function makeSprite(text, color, points) {
   
   texture = new THREE.Texture(canvas.node());
   texture.needsUpdate = true;
-
-  var url = canvas.node().toDataURL();
 
   canvas.remove();
 
@@ -36,7 +34,7 @@ export function makeSprite(text, color, points) {
   };
 }
 
-export var textCache = memoize(wrapText);
+export var getText = memoize(wrapText);
 
 export function wrapText(text, color, points, maxWidth) {
 
@@ -47,37 +45,49 @@ export function wrapText(text, color, points, maxWidth) {
   var testLine, testWidth;
 
   var pad = points * 0.5;
-  var lineHeight = points;
+  var lineHeight = points + pad;
 
   var total = lineHeight;
 
   canvas.attr({
-    width: maxWidth + (pad * 2),
-    height: 200
+    width: maxWidth + (pad * 2)
   });
 
   context = canvas.node().getContext("2d");
-  context.font = "normal " + points + "pt Arial";
+  context.font = "normal " + points + "pt arial";
+  context.textBaseline = "bottom";
   context.fillStyle = color;
 
-  var words = text.split(" ")
-  var line  = "";
+  var line  = "", lines = [], words = text.split(" ")
 
   for(var n = 0; n < words.length; n++) {
 
     testLine  = line + words[n] + " ";
     testWidth = context.measureText(testLine).width;
 
-    if(testWidth > maxWidth) {
-      context.fillText(line, pad, total);
+    if (testWidth > maxWidth) {
+      lines.push([line, pad, total]);
       line = words[n] + " ";
       total += lineHeight;
-    }
-    else {
+    } else {
       line = testLine;
     }
   }
-  context.fillText(line, pad, total);
+
+  lines.push([line, pad, total]);
+
+  canvas.attr({ // RESIZE THEN DRAW
+    height: total
+  });
+
+  context = canvas.node().getContext("2d");
+  context.font = "normal " + points + "pt arial";
+  context.textBaseline = "bottom";
+  context.fillStyle = color;
+
+  for (var i = 0; i < lines.length; i++) {
+    context.fillText(lines[i][0], lines[i][1], lines[i][2]);
+  }
 
   texture = new THREE.Texture(canvas.node());
   texture.needsUpdate = true;
