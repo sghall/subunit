d71911ae790b9bf4c28d5ac86d32f2bf84709d4b
Steven Hall <hall.steven.g@gmail.com>
2015-04-24 23:14:25 -0700

    update html

--- demos/modules/scene.js
+++ demos/modules/scene.js
@@ -1,8 +1,6 @@
-
-export var canvas = d3.select("body").append("canvas")
+export var canvas = d3.select("#canvas").append("canvas")
   .attr("width", window.innerWidth)
   .attr("height", window.innerHeight)
-  .style("opacity", 0)
   .style('background-color', '#EAEAEA');
 
 canvas.node().getContext("webgl");
@@ -33,21 +31,5 @@ function onWindowResize() {
   camera.updateProjectionMatrix();
   renderer.setSize( window.innerWidth, window.innerHeight );
 }
-
-// d3.select("#loading")
-//   .append("svg").attr({width: 100, height: 100})
-//   .style("margin-top", "-60px")
-//   .style("margin-left", "-20px")
-//   .append("circle").attr({cx: 50, cy: 50, r: 50}).style("opacity", 0)
-//   .transition().duration(6000).attr("r", 0).style("opacity", 1).remove();
-
-d3.select("body")
-  .append("div")
-  .html('<a href="https://github.com/sghall/subunit"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"></a>');
-
-
-
-
-
 
 
--- demos/modules/spiral-circle.js
+++ demos/modules/spiral-circle.js
@@ -1,10 +1,10 @@
-import { camera, scene, canvas, renderer, stats } from 'modules/scene';
-import { subunit } from '../src/index';
+import { camera, scene, canvas, renderer, stats } from './scene';
+import { SubUnit } from '../../src/index';
 
 var width = 960,
     height = 500,
     circleRadius = 180,
-    squareCount = 200,
+    squareCount = 20,
     squareSize = 78,
     speed = .08;
 
@@ -18,79 +18,69 @@ var square = d3.selectAll("g")
     .attr({width: squareSize, height: squareSize, x: -squareSize / 2, y: -squareSize / 2});
 
 console.log("square", square)
-// d3.timer(function(elapsed) {
-//   square
-//       .attr("transform", function(t) { return "rotate(" + (t * 360) + ")translate(0," + circleRadius + ")rotate(" + (t * 360 + elapsed * speed) + ")"; });
-// });
+d3.timer(function(elapsed) {
+  square
+      .attr("transform", function(t) { return "rotate(" + (t * 360) + ")translate(0," + circleRadius + ")rotate(" + (t * 360 + elapsed * speed) + ")"; });
+});
 
+square.each(function (d) {
+  console.log("loop", d);
+})
 
+console.log("square-svg", window.square = square);
       // import { camera, scene, canvas, renderer, stats } from 'modules/scene';
 
-      var width        = 960;
-      var height       = 500;
-      var circleRadius = 180;
-      var squareCount  = 200;
-      var squareSize   = 78;
-      var speed        = 0.08;
 
-      var root = SubUnit.select(scene);
-      var container = root.append("object");
+//       var width        = 960;
+//       var height       = 500;
+//       var circleRadius = 180;
+//       var squareCount  = 20;
+//       var squareSize   = 780;
+//       var speed        = 0.08;
 
-      var squares = container.selectAll(".frames")
-        .data([0,1]).enter()
-        .append("object")
-        .tagged("frame", true)
-        .each(function (d, i) {
-          this.rotation.z = i ? Math.PI: 0;
-        })
-        .selectAll("rect")
-          .data(d3.range(squareCount))
-        .enter().append("mesh")
-        .datum(function(i) { return i / squareCount; })
-      //   .attr({
-      //     width: squareSize, 
-      //     height: squareSize, 
-      //     x: -squareSize / 2, 
-      //     y: -squareSize / 2
-      //   });
-          // .append("mesh")
-          // .attr("tags", "bar")
-          // .attr("material", color1)
-          // .attr("geometry", function (d) {
-          //   var w = x.rangeBand();
-          //   var h = size[1] - y(d.frequency);
-          //   return new THREE.BoxGeometry(w, h, 5);
-          // })
-          // .each(function (d, i) {
-          //   var x0 = x(i) + x.rangeBand() / 2;
-          //   var y0 = -y(d.frequency) / 2;
-          //   this.position.set(x0, y0, 0);
-          // })
+//       var options = {color: 'tomato', side: THREE.DoubleSide};
+//       var material = new THREE.MeshLambertMaterial(options);
+//       var geometry = new THREE.PlaneBufferGeometry(squareSize, squareSize);
+
+//       var root = SubUnit.select(scene);
+//       var container = root.append("object");
+
+//       var frames = container.selectAll(".frame")
+//         .data([0,1]).enter()
+//         .append("object")
+//         .tagged("frame", true)
+//         .each(function (d, i) {
+//         })
+
+//       var squares = frames.selectAll("rect")
+//           .data(d3.range(squareCount))
+//         .enter().append("mesh")
+//         .datum(function(i) { return i / squareCount; })
+//         .attr("material", material)
+//         .attr("geometry", geometry)
+//         .each(function (d, i) {
+//           this.position.z = -i * 50
+//         });
 
 
-        // d3.timer(function(elapsed) {
-        //   squares
-        //       .attr("transform", function(t) { return "rotate(" + (t * 360) + ")translate(0," + circleRadius + ")rotate(" + (t * 360 + elapsed * speed) + ")"; });
-        // });
+// // d3.timer(function(elapsed) {
+// //   square
+// //       .attr("transform", function(t) { return "rotate(" + (t * 360) + ")translate(0," + circleRadius + ")rotate(" + (t * 360 + elapsed * speed) + ")"; });
+// // });
 
 
-        // var cntrl = new THREE.TrackballControls(camera, renderer.domElement);
-        // cntrl.rotateSpeed = 0.5;
-        // cntrl.minDistance = 100;
-        // cntrl.maxDistance = 6000;
+//         var controls = new THREE.TrackballControls(camera, renderer.domElement);
+//         controls.rotateSpeed = 0.5;
+//         controls.minDistance = 100;
+//         controls.maxDistance = 6000;
 
-        console.log("root: ", window.squares = squares, root)
+//         console.log("root: ", window.squares = squares, root)
 
-        // function animate() {
-        //   stats.update();
-        //   cntrl.update();
+//         function animate() {
+//           requestAnimationFrame(animate);
+//           renderer.render(scene, camera);
+//           stats.update();
+//           controls.update();
+//         }
 
-        //   // charts.each(function () {
-        //   //   this.rotation.x += 0.005;
-        //   // })
-
-        //   requestAnimationFrame(animate);
-        //   renderer.render(scene, camera);
-        // }
-
-        // animate();
+//         animate();
