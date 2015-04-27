a7028ac33c647ba2d21e6265c76527dc82dcf11b
Steven Hall <hall.steven.g@gmail.com>
2015-04-26 23:27:53 -0700

    update common methods

---
+++ src/common/append.js
@@ -0,0 +1,36 @@
+import THREE from 'THREE';
+
+export function append(name) {
+  name = selectionCreator(name);
+
+  return this.select(function () {
+    return name.apply(this, arguments);
+  });
+}
+
+function selectionCreator(name) {
+  var Func;
+
+  if (typeof name === "function") {
+    Func = name; // SEND ANY CONSTRUCTOR
+  } else if (name === "mesh") {
+    Func = THREE.Mesh;
+  } else if (name === "line") {
+    Func = THREE.Line;
+  } else if (name === "object") {
+    Func = THREE.Object3D;
+  } else if (name === "g") {
+    Func = THREE.Object3D;
+  } else {
+    throw new Error("Cannot append: ", name);
+  }
+
+  return function (data) {
+    var node = new Func();
+    node.__data__  = data;
+    node.__tags__  = [];
+    node.parentNode = this;
+    this.children.push(node);
+    return node;
+  };
+}
---
+++ src/common/use.js
@@ -0,0 +1,16 @@
+export function use(model, callback) {
+  return selectionUse(this, function(node, i, j) {
+    callback.call(model, node.__data__, i, j);
+  });
+}
+
+function selectionUse(groups, callback) {
+  for (var j = 0, m = groups.length; j < m; j++) {
+    for (var group = groups[j], i = 0, n = group.length, node; i < n; i++) {
+      if (node = group[i]) {
+        callback(node, i, j);
+      }
+    }
+  }
+  return groups;
+}
