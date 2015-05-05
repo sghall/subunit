a3fdcbc03deda4552099745e65c28c12f10ed6ed
Steven Hall <hall.steven.g@gmail.com>
2015-05-04 22:38:52 -0700

    clean up karma.conf

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,23 +1,18 @@
 module.exports = function(config){
   config.set({
-
     basePath : '../',
-
     files : [
       'node_modules/babel-core/browser-polyfill.js',
       'node_modules/babel-core/browser.js',
       'node_modules/three/three.js',
       'node_modules/d3/d3.js'
     ],
-
     systemjs: {
       files: [
         'src/**/*.js',
         'test/unit/**/*.js'
       ],
-
       configFile: 'test/system.config.js',
-
       config: {
         map: {
           d3: 'node_modules/d3/d3',
@@ -26,23 +21,17 @@ module.exports = function(config){
           SubUnit: 'src/index'
         }
       },
-
       testFileSuffix: '.spec.js'
     },
-
     autoWatch : true,
-
     frameworks: ['systemjs', 'jasmine'],
-
     browsers : ['Chrome'],
-
     plugins : [
-            'karma-systemjs',
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
-            'karma-jasmine'
-            ],
-
+      'karma-systemjs',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-jasmine'
+    ],
     junitReporter : {
       outputFile: 'test_out/unit.xml',
       suite: 'unit'
