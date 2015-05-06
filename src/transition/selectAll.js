555b69f2282e0e82fe217bfc907d91043fe92ef5
Steven Hall <hall.steven.g@gmail.com>
2015-05-05 21:37:34 -0700

    refactor transition.selectAll

--- src/transition/selectAll.js
+++ src/transition/selectAll.js
@@ -1,31 +1,32 @@
-import "../selection/selectAll";
-import "transition";
+import { selectionSelectorAll } from '../selection/selectAll';
+import { transitionNode } from './transitionNode/transitionNode';
+import { Transition } from '../core/Transition';
 
-d3_transitionPrototype.selectAll = function(selector) {
-  var id = this.id,
-      ns = this.namespace,
-      subgroups = [],
-      subgroup,
-      subnodes,
-      node,
-      subnode,
-      transition;
+export function selectAll(selector) {
+  var id = this.id;
+  var ns = this.namespace;
+  var subgroups = [], subgroup;
+  var subnodes, subnode, node;
+  var transition;
 
-  selector = d3_selection_selectorAll(selector);
+  selector = selectionSelectorAll(selector);
 
-  for (var j = -1, m = this.length; ++j < m;) {
-    for (var group = this[j], i = -1, n = group.length; ++i < n;) {
+  for (var j = -1, m = this.length; ++j < m; ) {
+    for (var group = this[j], i = -1, n = group.length; ++i < n; ) {
       if (node = group[i]) {
         transition = node[ns][id];
         subnodes = selector.call(node, node.__data__, i, j);
         subgroups.push(subgroup = []);
-        for (var k = -1, o = subnodes.length; ++k < o;) {
-          if (subnode = subnodes[k]) d3_transitionNode(subnode, k, ns, id, transition);
+        for (var k = -1, o = subnodes.length; ++k < o; ) {
+          if (subnode = subnodes[k]) {
+            transitionNode(subnode, k, ns, id, transition);
+          }
           subgroup.push(subnode);
         }
       }
     }
   }
 
-  return d3_transition(subgroups, ns, id);
-};
+  return Transition.from(subgroups, ns, id);
+}
+
