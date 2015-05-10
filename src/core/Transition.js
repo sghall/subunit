7018c1ffac0c4659aec102d8d11b7057eda68b75
Steven Hall <hall.steven.g@gmail.com>
2015-05-10 15:20:39 -0700

    update transition methods

--- src/core/Transition.js
+++ src/core/Transition.js
@@ -3,6 +3,9 @@ import { tagged } from '../selection/tagged';
 import { remove } from '../selection/remove';
 import { duration } from '../transition/duration';
 import { delay } from '../transition/delay';
+import { attr } from '../transition/attr';
+import { ease } from '../transition/ease';
+import { transition } from '../transition/transition';
 
 export class Transition extends SubUnitArray {
   constructor(input) {
@@ -14,3 +17,6 @@ Transition.prototype.tagged = tagged;
 Transition.prototype.duration = duration;
 Transition.prototype.delay = delay;
 Transition.prototype.remove = remove;
+Transition.prototype.attr = attr;
+Transition.prototype.ease = ease;
+Transition.prototype.transition = transition;
