da62e0b72c2d71cff779b340aa4c7d225687838b
Steven Hall <hall.steven.g@gmail.com>
2015-05-11 20:38:38 -0700

    updates to canvas text demo

--- demos/js/text-canvas-quotes.js
+++ demos/js/text-canvas-quotes.js
@@ -16,27 +16,22 @@ d3.csv('data/quotes.csv', function (err, data) {
   d3.select("#loading").remove();
 
   var root = SubUnit.select(scene)
-    .each(function () {
-      this.scale.set(0.01, 0.01, 0.01);
+    .attr("scale", {x: 0.005, y: 0.005, z: 0.005});
 
-      new TWEEN.Tween(this.scale)
-        .delay(500)
-        .to({x: 0.275, y: 0.275, z: 0.275}, 1000)
-        .start();
-    });
+  root.transition().duration(2000)
+    .attr("scale", {x: 0.275, y: 0.275, z: 0.275});
 
   var nodes = root.selectAll("node")
     .data(data).enter()
     .append("object")
-    .attr("tags", "node")
-    .each(function (d, i) {
-      this.position.copy(sphere(i, data.length, 3000));
-      this.lookAt(root.node().position);
-      this.rotation.y += Math.PI;
-    });
+    .attr("position", function (d, i) {
+      return sphere(i, data.length, 3000);
+    })
+    .attr("lookAt", root.node().position);
 
   var quotes = nodes.append("mesh")
     .attr("tags", "quote")
+    .attr("rotation", {y: Math.PI})
     .each(function (d) {
       var quote = getText(d.quote, "#ff0004", 20, cardWidth);
 
@@ -50,59 +45,63 @@ d3.csv('data/quotes.csv', function (err, data) {
     })
     .on("click", function (event, d, i) {
 
-      new TWEEN.Tween(this.rotation)
-        .to({x: Math.PI * 2}, 1000)
-        .onComplete(function () {
-          this.x = 0;
-        })
-        .start();
+      SubUnit.object(this).transition().duration(1000)
+        .attr("rotation", {x: Math.PI * 2});
+      // new TWEEN.Tween(this.rotation)
+      //   .to({x: Math.PI * 2}, 1000)
+      //   .onComplete(function () {
+      //     this.x = 0;
+      //   })
+      //   .start();
 
-      var temp = new THREE.Mesh();
-      temp.position.copy(sphere(i, data.length, 1000));
-      temp.lookAt(root.node().position);
+      // var temp = new THREE.Mesh();
+      // temp.position.copy(sphere(i, data.length, 1000));
+      // temp.lookAt(root.node().position);
 
-      new TWEEN.Tween(camera.position)
-        .to(temp.position, 1000)
-        .start();
+      // new TWEEN.Tween(camera.position)
+      //   .to(temp.position, 1000)
+      //   .start();
     });
 
-  var names = nodes.append("mesh")
-    .attr("tags", "name")
-    .each(function (d) {
-      var quote = getText(d.quote, "#ff0004", 20, cardWidth); // FROM CACHE
-      var label = getLabel("-" + d.name, "#fff", 30);
+  // var names = nodes.append("mesh")
+  //   .attr("tags", "name")
+  //   .each(function (d) {
+  //     var quote = getText(d.quote, "#ff0004", 20, cardWidth); // FROM CACHE
+  //     var label = getLabel("-" + d.name, "#fff", 30);
 
-      this.material = new THREE.MeshBasicMaterial({
-        map: label.map,
-        side: THREE.DoubleSide,
-        transparent: true
-      });
+  //     this.material = new THREE.MeshBasicMaterial({
+  //       map: label.map,
+  //       side: THREE.DoubleSide,
+  //       transparent: true
+  //     });
 
-      this.geometry = new THREE.PlaneBufferGeometry(label.width, label.height);
+  //     this.geometry = new THREE.PlaneBufferGeometry(label.width, label.height);
 
-      var x = quote.width / 2 - label.width / 2;
-      var y = -quote.height / 2 - label.height;
+  //     var x = quote.width / 2 - label.width / 2;
+  //     var y = -quote.height / 2 - label.height;
 
-      this.position.set(x, y, 0);
-    })
-    .on("click", function () {
-      new TWEEN.Tween(this.rotation)
-        .to({z: 2 * Math.PI}, 500)
-        .onComplete(function () {
-          this.z = 0;
-        })
-        .start();
+  //     this.position.set(x, y, 0);
+  //   })
+  //   .on("click", function () {
+  //     new TWEEN.Tween(this.rotation)
+  //       .to({z: 2 * Math.PI}, 500)
+  //       .onComplete(function () {
+  //         this.z = 0;
+  //       })
+  //       .start();
 
-      var parent = this.parentNode;
+  //     var parent = this.parentNode;
 
-      new TWEEN.Tween(parent.rotation)
-        .to({z: parent.rotation.z + Math.PI / 2}, 200)
-        .start();
+  //     new TWEEN.Tween(parent.rotation)
+  //       .to({z: parent.rotation.z + Math.PI / 2}, 200)
+  //       .start();
 
-    });
+  //   });
 
   raycast(camera, quotes[0], 'click');
-  raycast(camera, names[0], 'click');
+  // raycast(camera, names[0], 'click');
+
+  console.log("root", window.root = root[0]);
 
   function animate() {
     requestAnimationFrame(animate);
