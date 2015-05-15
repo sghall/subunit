56b905d91aee5d3a3499a543573a2174f63fd73d
Steven Hall <hall.steven.g@gmail.com>
2015-05-14 22:17:21 -0700

    add spiral circle leaf demo

---
+++ demos/js/spiral-circle-v2.js
@@ -0,0 +1,80 @@
+import d3 from 'd3';
+import THREE from 'THREE';
+import { SubUnit } from 'SubUnit';
+import { camera, scene, renderer } from './common/scene';
+import './common/OrbitControls';
+import './common/EffectComposer';
+import './common/CopyShader';
+import './common/MirrorShader';
+import './common/RenderPass';
+import './common/ShaderPass';
+import './common/MaskPass';
+
+var circleRadius = 180;
+var squareCount  = 100;
+var squareSize = 150;
+
+var speed = 0.0005;
+
+var metal = THREE.ImageUtils.loadTexture('images/leaf.png', null);
+
+var options  = {map: metal, transparent: true};
+var material = new THREE.MeshPhongMaterial(options);
+var geometry = new THREE.PlaneBufferGeometry(squareSize, squareSize);
+
+d3.select("#loading").transition().duration(500)
+  .style("opacity", 0).remove();
+
+var root = SubUnit.select(scene)
+  .attr("scale", {x: 0.1, y: 0.1, z: 0.1});
+
+root.transition().duration(4000).ease("bounce")
+  .attr("scale", {x: 3, y: 3, z: 3});
+
+var containers = root.selectAll("rect")
+  .data(d3.range(squareCount))
+  .enter().append("object")
+  .datum(function(i) { return i / squareCount; })
+  .attr("rotation", function (d) {
+    return {z: d * Math.PI * 2};
+  })
+  .attr("translation", function (d, i) {
+    return {x: circleRadius, z: (i / squareCount) * 5};
+  });
+
+containers.append("mesh")
+  .datum(function(i) { return i / squareCount; })
+  .attr("material", material)
+  .attr("geometry", geometry)
+  .attr("translation", function () {
+    return {z: Math.PI / 5};
+  });
+
+d3.timer(function(elapsed) {
+  containers.each(function (t) {
+    this.rotation.z = -(t * Math.PI * 2 + (elapsed * speed));
+  });
+});
+
+var renderPass = new THREE.RenderPass(scene, camera);
+var mirrorPass = new THREE.ShaderPass(THREE.MirrorShader);
+
+var composer = new THREE.EffectComposer(renderer);
+composer.addPass(renderPass);
+composer.addPass(mirrorPass);
+
+mirrorPass.renderToScreen = true;
+mirrorPass.uniforms.side.value = 0;
+
+console.log("root: ", window.root = root, root);
+
+var control = new THREE.OrbitControls(camera, renderer.domElement);
+control.noRotate = true;
+
+function animate() {
+  control.update();
+  composer.render(0.1);
+  requestAnimationFrame(animate);
+}
+
+animate();
