88bba2e93e260e2aa16256217513bd78da858b90
Steve Hall <hall.steven.g@gmail.com>
2017-06-02 23:28:18 -0700

    clean up select

--- src/selection/select.js
+++ src/selection/select.js
@@ -1,19 +1,27 @@
-import { search } from "../utils/utils";
-import { Selection } from "../Selection";
+// @flow weak
+/* eslint no-use-before-define: "off", no-underscore-dangle: "off" */
 
-export function select(selector) {
-  var subgroups = [], subgroup, subnode, group, node;
+import { search } from '../utils/utils';
+import { Selection } from '../Selection';
+
+export default function select(selector) {
+  const subgroups = [];
+
+  let subgroup;
+  let subnode;
+  let group;
+  let node;
 
   selector = selectionSelector(selector);
 
-  for (var j = -1, m = this.length; ++j < m; ) {
+  for (let j = -1, m = this.length; ++j < m;) {
     subgroups.push(subgroup = []);
     subgroup.parentNode = (group = this[j]).parentNode;
 
-    for (var i = -1, n = group.length; ++i < n; ) {
-      if (node = group[i]) {
+    for (let i = -1, n = group.length; ++i < n;) {
+      if (node = group[i]) { // eslint-disable-line no-cond-assign
         subgroup.push(subnode = selector.call(node, node.__data__, i, j));
-        if (subnode && "__data__" in node) {
+        if (subnode && '__data__' in node) {
           subnode.__data__ = node.__data__;
         }
       } else {
@@ -26,7 +34,7 @@ export function select(selector) {
 }
 
 function selectionSelector(selector) {
-  return typeof selector === "function" ? selector : function() {
+  return typeof selector === 'function' ? selector : function searchSelection() {
     return search(this, selector);
   };
 }
