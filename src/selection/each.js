70a615aff636ea16704ccd1d1e72c519b8068229
Steve Hall <hall.steven.g@gmail.com>
2017-06-03 17:29:50 -0700

    add selection each

---
+++ src/selection/each.js
@@ -0,0 +1,17 @@
+
+export default function each(callback) {
+  return selectionEach(this, function (node, i, j) {
+    callback.call(node, node.__data__, i, j);
+  });
+}
+
+export function selectionEach(groups, callback) {
+  for (let j = 0, m = groups.length; j < m; j++) {
+    for (let group = groups[j], i = 0, n = group.length, node; i < n; i++) {
+      if (node = group[i]) {
+        callback(node, i, j);
+      }
+    }
+  }
+  return groups;
+}
