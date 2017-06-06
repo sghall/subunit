5f530ab66d2fa9f8c61ca4d8c6443527fc21f9ab
Steve Hall <hall.steven.g@gmail.com>
2017-06-05 23:45:47 -0700

    clean up/delete old files

--- src/Selection.js
+++ src/Selection.js
@@ -1,4 +1,3 @@
-// import BaseSelection from './BaseSelection';
 import filter from './selection/filter';
 import data from './selection/data';
 import sort from './selection/sort';
@@ -20,29 +19,10 @@ import each from 'd3-selection/src/selection/each';
 import property from 'd3-selection/src/selection/property';
 import datum from 'd3-selection/src/selection/datum';
 
-// import data from './selection/data';
-// import attr from './selection/attr';
-// import transition from './selection/transition';
-// import selectAll from './selection/selectAll';
-// import select from './selection/select';
-// import append from './common/append';
-
-
 export default function Selection(groups, parents) {
   this._groups = groups;
   this._parents = parents;
 }
-
-// function selection() {
-//   return new Selection([[document.documentElement]], root);
-// }
-
-
-// export default class Selection extends BaseSelection {
-//   constructor(input) {
-//     super(input);
-//   }
-// }
 
 Selection.prototype.filter = filter;
 Selection.prototype.property = property;
@@ -64,10 +44,3 @@ Selection.prototype.selectAll = selectAll;
 Selection.prototype.transition = transition;
 Selection.prototype.node = node;
 Selection.prototype.nodes = nodes;
-
-// Selection.prototype.data = data;
-// Selection.prototype.attr = attr;
-// Selection.prototype.transition = transition;
-// Selection.prototype.selectAll = selectAll;
-// Selection.prototype.select = select;
-// Selection.prototype.append = append;
